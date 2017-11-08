0902d2d0045bd3c14432e90743e6e4699c106e80
Gatis Jansons <jansons.gatis@gmail.com>
2017-11-08 18:08:28 +0200

    Projectpage

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -42,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
